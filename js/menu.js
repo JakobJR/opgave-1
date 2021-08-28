a9ed158a905b30936d13c091f78b0e0b314788c2
JakobJR <jakobjr@hotmail.com>
2021-08-28 22:48:30 +0200

    Nav responsive
    - nav bar is fully responsive for tablet
    - setup helper classes for making remaining sections responsive
    - done a bit of JS for the mobile menu
    - created mixings for breakpoints
    
    TODO: nav responsive mobile + rest of the site

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.querySelector('#mobile-menu-btn').addEventListener('click', () => {
+    document.querySelector('#bottom').classList.toggle('mobile-menu-height');
+});
+
+document.querySelector('#mobile-menu-btn').addEventListener('click', () => {
+    document.querySelector('#mobile-menu-icon').classList.toggle('spin');
+});
