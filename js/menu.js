6a86fcb08ce83f4fc1857d03bf01261d14df101e
JakobJR <jakobjr@hotmail.com>
2021-08-29 18:22:44 +0200

    Few tweaks to icons in nav
    - some additional edits to JS to keep it mobile friendly

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,11 @@ document.querySelector('#mobile-menu-btn').addEventListener('click', () => {
 document.querySelectorAll('.icon-wrapper').forEach(elem => elem.addEventListener("click", function(event) {
     event.target.nextElementSibling.classList.toggle('show-info');
 }));
+
+document.querySelector('.fa-envelope').addEventListener('click', () => {
+    document.querySelector('.tlf').classList.remove('show-info');
+});
+
+document.querySelector('.fa-phone').addEventListener('click', () => {
+    document.querySelector('.top-mail').classList.remove('show-info');
+});
